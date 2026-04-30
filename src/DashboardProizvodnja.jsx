d6a8f03b8c6c7f57e1824c57d61412c61037e9d4
NikolaDjuricic1504 <n.djuricic@maropack.rs>
2026-04-30 13:20:30 +0200

    Delete src directory

--- src/DashboardProizvodnja.jsx
+++
@@ -1,4 +0,0 @@
-import React from "react";
-export default function DashboardProizvodnja() {
-  return <div className="card"><h2>Dashboard proizvodnja</h2><p><span className="badge">Radi</span> Osnovni pregled sistema.</p></div>;
-}
